8e2e096d135172d6c46f6b157af3a9bc11cbd67b
David Pérez Contreras <david.perez.dam2@gmail.com>
2021-09-29 09:56:09 +0200

    La entrada funciona, la salida no

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,13 @@
+
+
+//Agarra el contenido del archivo Entrada.txt
+//Pide una serie de letras para eliminarlas de la cadena
+//Luego escibe el output en el archivo Salida.txt
+
+
+
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 public class Main {
 
@@ -13,19 +23,38 @@ public class Main {
 		Scanner teclado = new Scanner(System.in);
 		String frase="";
 		String letra="";
+
+		try {
+			FileReader input = new FileReader("src\\Entrada.txt");
+			int c=input.read();
+		
+			frase+=(char)input.read();
+			
+			while(c!=-1) {
+				frase+=(char)c;
+				c=input.read();
+				
+			
+			}
+			input.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(frase);
 		
 		//Pide al usuario que escriba una frase
-		System.out.println("Escribe una frase");
-		frase = teclado.nextLine();
+		//System.out.println("Escribe una frase");
+		//frase = teclado.nextLine();
 		
-		//Pide al usuario que escriba una letra
+		//Pide al usuario que escriba una cadena
 		System.out.println("Escribe una letra o frase para eliminarla");
 		letra = teclado.nextLine();
 		
 		
-		teclado.close();
+		//teclado.close();
 		//reemplaza la letra con una cadena vacía (no un espacio)
-		frase=frase.replace(letra, "");
+		//frase=frase.replace(letra, "");
 		
 		
 		
