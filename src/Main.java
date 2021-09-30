d7e97b271559f6c6cffd4cb84a03d8f6377db1cb
David Pérez Contreras <david.perez.dam2@gmail.com>
2021-09-30 09:00:58 +0200

    Funciona. distingue mayusculas y minusculas

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,11 @@
 //Agarra el contenido del archivo Entrada.txt
 //Pide una serie de letras para eliminarlas de la cadena
 //Luego escibe el output en el archivo Salida.txt
-
+//Lee el contenido de un archivo,  pregunta una cadena, y escribe el contenido en un archivo de salida quitando los carácteres que aparecen en esa cadena
 
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 public class Main {
@@ -22,13 +23,18 @@ public class Main {
 		//Inicializar variables
 		Scanner teclado = new Scanner(System.in);
 		String frase="";
-		String letra="";
-
+		String letras;
+		int c;
+		Character [] letrasParaEliminar;
+		
+		
+		
+		//ENTRADA
 		try {
 			FileReader input = new FileReader("src\\Entrada.txt");
-			int c=input.read();
+			c=input.read();
 		
-			frase+=(char)input.read();
+			//frase+=(char)input.read();
 			
 			while(c!=-1) {
 				frase+=(char)c;
@@ -36,31 +42,58 @@ public class Main {
 				
 			
 			}
-			input.close();
+		input.close();
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
+		//Imprime la frase de ENTRADA
 		System.out.println(frase);
 		
-		//Pide al usuario que escriba una frase
-		//System.out.println("Escribe una frase");
-		//frase = teclado.nextLine();
-		
-		//Pide al usuario que escriba una cadena
-		System.out.println("Escribe una letra o frase para eliminarla");
-		letra = teclado.nextLine();
 		
 		
-		//teclado.close();
-		//reemplaza la letra con una cadena vacía (no un espacio)
-		//frase=frase.replace(letra, "");
 		
 		
 		
 
 		
+		//Pide al usuario que escriba una cadena
+		System.out.println("Escribe una letra o frase para eliminarla");
+		letras = teclado.nextLine();
+		teclado.close();
+		
+		//Rellena el Array de carácteres con las letras de la frase
+		letrasParaEliminar = new Character[letras.length()];
+		for (int i=0;i<letras.length();i++) {
+			letrasParaEliminar[i]=letras.charAt(i);
+		}
+		
+		
+		for(Character i:letrasParaEliminar) {
+			frase=frase.replace(Character.toString(i), "");
+			System.out.println(i);
+		}
+		
+		
+		
+		
+		//La muestra por pantalla, pero quiero que lo escriba en un archivo
 		System.out.println(frase);
+		
+		
+		try {
+			FileWriter output = new FileWriter("src\\Salida.txt");
+			
+			output.write(frase);
+			
+			output.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 	}
